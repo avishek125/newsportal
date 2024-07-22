2e54db4af21e30a853a3e04cf0c8e3b7c3d5c5a4
avishek125 <abishekmalla50@gmail.comm>
2024-07-22 19:26:35 +0545

    trending_news & popular news & rec news

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -20,6 +21,7 @@ class News(models.Model):
     description = models.TextField()
     is_editorial =  models.BooleanField(default=False)
     views_count = models.PositiveIntegerField(default=0)
+    posted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="newsrepoter")
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
--- news/views.py
+++ news/views.py
@@ -5,8 +5,15 @@ from news.models import News
 home page
 """
 def home_page(request):
+    editorial_news = News.objects.filter(is_editorial=True).order_by("-id")[:3]
+    trending_news = News.objects.all().order_by("-views_count")[:4]
+    popular_news = News.objects.all().order_by("-views_count")[:4]
+    recent_news = News.objects.all().order_by("-created_date")[:3]
     context = {
-        "news":"",
+        "editorial":editorial_news,
+         "trending_news": trending_news,
+         "popular_news":popular_news,
+         "recent_news":recent_news,
     }
     return render(request,'index.html', context)
 
